71bd071c3816070733c52fe67886a39ac2e82a24
limgeontae <taelim@ilark.co.kr>
2024-02-14 15:01:21 +0900

    에러처리

--- src/hooks/useGetWeather.ts
+++ src/hooks/useGetWeather.ts
@@ -2,6 +2,7 @@ import { useQuery } from 'react-query'
 import { useGeoLocation } from './useGeoLocation'
 import icAxios from '../util/icAxios'
 import { locationInfo } from '../types/page/userMainType'
+import Swal from 'sweetalert2'
 
 
 const fetchWeather = async (location : locationInfo | undefined) => {
@@ -23,6 +24,15 @@ export const useGetWeather = () => {
     maximumAge: 7000,
   }
   const { location, error } = useGeoLocation(geolocationOptions);
+
+  if(error){
+    console.log(error)
+    Swal.fire({
+      icon: 'error',
+      title: '위치정보 오류',
+      text: error,
+    })
+  }
 
   const { isLoading, isError, data } = useQuery(
     "weather", ()=>fetchWeather(location), {
